455d580cfd1d7d1c3c999799c1e295d76391ebfe
Matias Moya <matymoya18@gmail.com>
2026-08-26 17:53:49 -0300

    Preparar despliegue web en Google Cloud Run

--- server.ts
+++ server.ts
@@ -46,7 +46,7 @@ function getOneDriveDirectUrl(shareUrl: string): string {
 }
 
 const app = express();
-const PORT = 3000;
+const PORT = Number(process.env.PORT) || 3000;
 
 app.use(express.json());
 
